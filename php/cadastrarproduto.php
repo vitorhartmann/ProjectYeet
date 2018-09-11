62a492bcc1a48d4da61f06365c4e9cc19d852866
vitorhartmann <32484028+vitorhartmann@users.noreply.github.com>
2018-09-11 16:46:21 -0300

    Vide Descrição
    Algumas coisas entraram, outras saíram, tentei ajeitar o banco de dados, ninguem ganhou mas ninguém perdeu, mas todos perderam

--- php/cadastrarproduto.php
+++ php/cadastrarproduto.php
@@ -1,26 +1,43 @@
 <?php
+
+
+include("conexao.php");
+
+
+
 // RECEBENDO OS DADOS PREENCHIDOS DO FORMULÁRIO !
-$codbarras_produto= $_POST ["codigobarras"];//atribuição do campo "codigobarras" vindo do formulário para variavel
-$nome_produto= $_POST ["nomedoproduto"];//atribuição do campo "nomedoproduto" vindo do formulário para variavel
+$codbarrasproduto= $_POST ["codigobarras"];//atribuição do campo "codigobarras" vindo do formulário para variavel
+$nomeproduto= $_POST ["nomedoproduto"];//atribuição do campo "nomedoproduto" vindo do formulário para variavel
 
  
-//Gravando no banco de dados ! conectando com o localhost - mysql
-$conexao = mysql_connect("localhost","root"); //localhost é onde esta o banco de dados.
-if (!$conexao)
-die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysql_error());
- 
-//conectando com a tabela do banco de dados
-$yeet = mysql_select_db("Produto",$conexao); //nome da tabela que deseja que seja inserida os dados cadastrais
-if (!$yeet)
-die ("Erro de conexão com banco de dados, o seguinte erro ocorreu -> ".mysql_error());
- 
- 
-//Query que realiza a inserção dos dados no banco de dados na tabela indicada acima
-$query = "INSERT INTO `produto` ( `codbarras_produto` , `nome_produto` )
-VALUES ('$codbarras_produto', '$nome_produto')";
-mysql_query($query,$conexao);
 
-#############################################
-echo "Produto Cadastrado com sucesso.";
-//mensagem que é escrita quando os dados são inseridos normalmente.
+
+ 
+$comando="INSERT INTO produto (nome_produto,codbarras_produto,) 
+	VALUES ('{$nomeproduto}','{$codbarrasproduto}')";
+	
+
+	
+$mysqli_query($comando);
+	
+// $resultado=mysqli_query($conexao,$comando);
+	
+	// if($resultado==true){
+		//echo "
+		//<script>
+			//alert('Produto cadastrado com sucesso!');
+			//location.href='../cadastrarproduto.html';
+		//</script>		
+		//";
+	//}else{
+		//echo "
+		//<script>
+			//alert('Erro ao cadastrar produto!');
+			//location.href='../cadastrarproduto.html';
+		//</script>		
+		//";
+	//} 
+
+
+
 ?>
---
+++ php/conexao.php
@@ -0,0 +1,7 @@
+<?php 
+	$conexao=mysqli_connect('localhost','root','root','yeet'); //SUBSTITUIR O # PELO NOME DO BANCO DE DADOS
+
+	//ESTABELECE UMA CONEXAO COM O BANCO DE DADOS
+
+	//A variavel conexão vai armazenar a conexao com o banco que a função mysqli_connect retornará 
+?>
