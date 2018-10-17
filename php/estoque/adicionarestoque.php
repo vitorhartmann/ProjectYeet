f40aae4f136c88928453343deb750dc9c230bf98
Vitor Hartmann <vitorhartmann@hotmail.com>
2018-10-17 16:02:28 -0300

    .

--- php/estoque/adicionarestoque.php
+++ php/estoque/adicionarestoque.php
@@ -2,4 +2,7 @@
     //Recebe os dados com as alterações feitas
 	include("../conexao.php");
     $id_produto = $_POST ["id_produto"];
+	$adicionar_produto = $_POST ["adicionar_produto"];
+	$novo_valor = $_POST ["novo_valor"];
     
+?>
