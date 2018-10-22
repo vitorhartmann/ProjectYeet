03eaa1d0bf7fdeae8e0ca4c3ca326410f47da6b7
vitor_hartmann@joinville.sc.senai.br <vitor_hartmann@joinville.sc.senai.br>
2018-10-22 14:07:51 -0300

    .

--- php/estoque/adicionarestoque.php
+++ php/estoque/adicionarestoque.php
@@ -86,21 +86,18 @@ $(document).ready(function() {
     $adicionar_produto = $_POST ["adicionar_produto"];
 	$novoValor=$_POST ["novo_valor"];
 	
-	$sql="Select * from produto where id_produto=" . $id_produto . "";
+	$sql='Select * from produto where id_produto=" . $id_produto . "';
 	
-	$saldo_produto= $_POST ["saldo_produto"];
+		mysqli_query($conn, $sql);
+	 while($resultado = mysqli_fetch_array($sql)){
 	
-	mysqli_query($conn, $sql);
+	$saldo_produto=$resultado["saldo_produto"];
+	 }
 	
 	
 	$saldototal_produto=$adicionar_produto+$saldo_produto ;
 
-	 if(isset($novoValor)){ 
-		 
-	$novovalor=$_POST ["novo_valor"];
-	 }else{
-		 $novoValor=$_POST["antigo_valor"];
-	 }
+
     
 	$sql = "UPDATE produto SET saldo_produto='" . $saldototal_produto . "', valor_produto='" . $novoValor . "'  WHERE id_produto=".$id_produto ;
 
