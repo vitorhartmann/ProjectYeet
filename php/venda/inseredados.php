737b0c9e7bfa78cb4805fa64d21d868012ec4ce6
vitorhartmann <32484028+vitorhartmann@users.noreply.github.com>
2018-10-31 14:46:56 -0300

    Muita Mágica

---
+++ php/venda/inseredados.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Exemplo Mestre-Detalhe</title>
+	</head>
+	<body>
+		<?php
+			/*
+				Esta página está incompleta. Favor focar no recebimento e validação dos dados do mestre-detalhe.
+			*/
+			$p_selproduto = $_POST['selproduto'];
+			$p_txtqtde = $_POST['txtqtde'];
+
+			$p_selproduto_validacao = in_array("", $p_selproduto);
+			$p_txtqtde_validacao = in_array("" , $p_txtqtde);
+
+			if($p_selproduto_validacao){
+				echo "Algum produto não foi selecionado!";
+			}else if($p_txtqtde_validacao){
+				echo "Alguma quantidade não foi preenchida!";
+			}else{
+				echo "Formulário preenchido por completo! <br /> Valores abaixo: <br /> <br /> ID Produto - Quantidade <br /> <br />";
+
+				$qtde_produtos = count($p_selproduto);
+				for($i=0; $i<$qtde_produtos; $i++){
+					echo $p_selproduto[$i]." - ".$p_txtqtde[$i]."<br />";
+				}
+			}
+		?>
+	</body>
+</html>
--- php/venda/teladevendas.php
+++ php/venda/teladevendas.php
@@ -1,4 +1,76 @@
+<?php
+  include ("../conexao.php");
+  //Define o gmt (timezone) que o servidor deve se basear para horários e datas. Isso pode ser feito diretamente dentro no php.ini.
+  date_default_timezone_set("America/Sao_Paulo");
+?>
 <html>
+<script  src="jquery.js"></script>
+    <script type="text/javascript">
+    //Abaixo o código Javascript com jQuery que permite remover e adicionar Detalhes do formulário.
+    //indica que apenas quando o documento estiver pronto (ready) execute as seguintes linhas dentro do bloco.
+    $(function() {
+
+    	//cria a função removeDetalhe
+      function removeDetalhe() {
+        //desvincula a ação de clique na class "removerDetalhe" (botão de X) que vem junto com o clone, para que posteriomente seja vinculada uma função de "click" que referencie a linha correta do elemento.
+        $(".removerDetalhe").off("click");
+        //ao se clicar no elemento com a class "removerDetalhe", inicia uma função em que...
+        $(".removerDetalhe").on("click", function() {
+          //... se a quantidade de tags tr com a classe detalhes for maior que 1, ou seja, se houver mais de uma linha no mestre-detalhe (isso serve para evitar que se exclua todas as linhas)
+          if($("tr.detalhes").length > 1){
+            //remove a linha que contém o botão de excluir que foi clicado
+            //explicando: this seria o botão (pois o clique nele causou a execução dessa função), o primeiro parent é a célula onde está o botão e o segundo parent é a linha onde está o botão, assim o remove age na linha
+            $(this).parent().parent().remove();
+          //senão
+          }else{
+            //avisa que não se pode excluir a última linha
+            alert("A última linha não pode ser removida.");
+          }
+          //termina a function do on
+          }
+        );
+      //termina a função removeDetalhe
+      }
+
+      //Ao clicar no botão adicionarDetalhe, inicia a seguinte função:
+      $(".adicionarDetalhe").click(function () {
+        //Cria um clone da primeira linha de detalhe e salva na variável novoDetalhe
+        novoDetalhe = $("tr.detalhes:first").clone();
+        //Esvazia os valores de todos os inputs do clone (já o select aparece vazio por ser carregado do banco novamente ao se criar a linha)
+        novoDetalhe.find("input").val("");
+        //Insere o clone na página, logo após a última linha já existente (LAST)
+        novoDetalhe.insertAfter("tr.detalhes:last");
+        //Executa a função removeDetalhe para que o detalhe inserido possa ser excluido com o clique no botão de menos dessa linha
+        //(PS: essa ação aqui é obrigatória, mas você pode também executar essa função acima de sua própria criação (linha 17) para que, caso a pessoa tente excluir a única linha logo ao carregar a página, apareça o alert da linha 32)
+        removeDetalhe();
+      //fecha a função da linha 41
+      });
+    //finaliza as funções de adicionar/remover detalhe
+    });
+
+
+    //função de validação simples dos Detalhes do mestre detalhe, onde se mostra a primeira linha vazia caso haja alguma
+    function validaDetalhe(){
+      //recebe os valores dos produtos preenchidos
+      var produtosValidar = document.getElementsByName('selproduto[]');
+      //recebe os valores das quantidades preenchidas
+      var qtdeValidar = document.getElementsByName('txtqtde[]');
+      //conta quantos elementos foram capturados
+      var qtdeElementos = produtosValidar.length;
+      //estrutura para-faça para repetir a validação enquanto i for menor que o tamanho do array
+      for (var i = 0;i < qtdeElementos; i++){
+        //se a posição atual dos arrays de produto e quantidade estiverem vazios,
+        if ((produtosValidar[i].value=="")||(qtdeValidar[i].value=="")){
+          //cria a variável linha com valor de "i mais um" para a mensagem avisar corretamente qual Detalhe não foi preenchido, já que o array começa da posição 0
+          var linha = i+1;
+          alert ("A linha "+linha+" não foi completamente preenchida.");
+          //foca o campo de produto da linha que não está devidamente preenchida
+          produtosValidar[i].focus();
+          return false;
+        }
+      }
+    }
+    </script>
 	<header>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="../../css/css.css">
@@ -22,82 +94,81 @@
 			<button><a href="../../index.html"><img src="../../img/homeicon.png" width="35px" height="35px"></a></button>
 			<button><a href="../../index.html"><img src="../../img/backicon.png" width="35px" height="35px"></a></button>
 		</div>
-		
-		<div id="tudo">
-			<table border="1" align="center" bgcolor="white">
-				<tr>
-				<td>Numero NF: 0001</td>
-				<td>Data: 01/01/2018</td>
-				</tr>
-			</table>
-			<br>
-			<table border="1" bgcolor="white" align="center" width="86%">
-				<center>
-					<tr>
-						<th>Cód. Barras</th>
-						<th>Nome</th>
-						<th>Preço Un.</th>
-						<th>Quant</th>
-						<th>Preço</th>
-						<th>Exclusão de Item</th>
-
-					</tr>
-				
-					<tr>
-						<th><input value="7896256600223"> </th>
-						<th> Leite Tirol 1L </th>
-						<th>3,00 </th>
-						<th><input value="2"> </th>
-						<th>6,00</th>
-						<th><img src="img/excluirbotao.png" height="20px" width="20px"></td>
-				
-				
-					</tr>
-					
-					<tr>
-						<th><input value="7897492500025"></input></th>
-						<th> Arroz Arrozeiro </th>
-						<th>2,00 </th>
-						<th><input value="5"></th>
-						<th>10,00</th>
-						<th><img src="img/excluirbotao.png" height="20px" width="20px"></td>
-				
-				
-					</tr>
-				</center>
-			
-				
-			</table>
-
-			
-		
-			<div id="valorfinal">
-				<table align="right" border="1" bgcolor="white">
-					<td><button><b>Adicionar Produto</b></button></td>
-					<td><h3>Total: R$ 16,00</h3></td>	
-				<table>
-			</div>
-		</div>
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	</body>
+		    <form name="frm_venda" method="post" action="inseredados.php" onsubmit="return validaDetalhe()">
+      <table border="0" bgcolor="#A9A9A9" align="center">
+        <tr>
+          <?php
+            /*
+              Desafio :D - na real foi só pra vc ficar pensando, "Como?!"
+            */
+            @exec('wmic COMPUTERSYSTEM Get UserName', $vendedor);
+            @$vendedor = explode("\\", $vendedor[1]);
+            @$vendedor = array_reverse($vendedor);
+            @$vendedor = explode("_", $vendedor[0]);
+          ?>
+          <td align="right">Vendedor: </td>
+          <td colspan="2">
+            <input type="text" name="txtvendedor" value="<?php echo @ucfirst($vendedor[0]).' '.@ucfirst($vendedor[1]); ?>">
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Data: </td>
+          <td colspan="2">
+            <input type="text" name="txtdata" value="<?php echo date("d/m/Y"); ?>">
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Hora: </td>
+          <td colspan="2">
+            <input type="text" name="txthora" value="<?php echo date("H:i:s"); ?>">
+          </td>
+        </tr>
+        <tr>
+          <td class="titulo_produtos" colspan="3">Produto(s)</td>
+        </tr>
+        <tr>
+          <td>Produto</td>
+          <td>Qtde</td>
+          <td>
+            <!--botão com função de adicionar uma nova linha de produto, ou seja, um novo "detalhe", a cada clique-->
+            <a href="#" class="adicionarDetalhe" title="Adicionar item"><img src="../../img/botaoadicionar.png" border="0" width="35px" height="35px"></a>
+          </td>
+        </tr>
+        <!--Linha do detalhe, identificada pela classe "detalhes"-->
+        <tr class="detalhes">
+          <td>
+            <select name="selproduto[]">
+              <!-- Por que array("[]")? Porque haverão vários "detalhes", ou seja, vários selects de produtos.
+              Sendo assim, todos os campos de um detalhe deve ter seus nomes como array-->
+              <option value="">Selecione um produto...</option>
+              <?php
+              $sql_slt_produtos="SELECT * FROM produto ORDER BY nome ASC";
+              $sql_slt_produtos_preparado=$conn->prepare($sql_slt_produtos);
+              $sql_slt_produtos_preparado->execute();
+              while($sql_slt_produtos_dados=$sql_slt_produtos_preparado->fetch()){
+                $codigo_produto=$sql_slt_produtos_dados['codbarras_produto'];
+                $nome_produto=$sql_slt_produtos_dados['nome_produto'];
+                echo "<option value='$codigo_produto'>$nome_produto</option>";
+              }
+              ?>
+            </select>
+          </td>
+          <td>
+            <input type="text" name="txtqtde[]" size="1">
+          </td>
+          <td>
+            <!--botão com função de remover essa linha de produto, ou seja, esse "detalhe", ao clicar-->
+            <a href="#" class="removerDetalhe" title="Remover linha"><img src="../../img/excluirbotao.png" border="0"></a>
+          </td>
+        </tr>
+        <tr>
+          <td align="right" colspan="3">
+            <button type="submit">Cadastrar</button>
+            <button type="reset">Limpar</button>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
 	
 </html>
