d6fc2457440cba49b770fb1b258dd19a0ae56ce2
vitorhartmann <32484028+vitorhartmann@users.noreply.github.com>
2018-11-12 16:17:43 -0200

    .

--- php/venda/buscaProduto.php
+++ php/venda/buscaProduto.php
@@ -1,5 +1,5 @@
 <?php
-include "conexao.php";
+include "../conexao.php";
 //recebe o código do estado selecionado
 $tipo=$_POST['pesquisarpor'];
 //se o código for vazio, quer dizer que a pessoa não escolheu um estado, então...
@@ -7,7 +7,12 @@ if ($tipo=="nome"){
 	//através do echo, retorna para a página index uma option que diz que a pessoa deve selecionar o estado
 	
 	$sql="SELECT * FROM produto WHERE nome_produto = '$nome_produto'"
-	echo ;
+	$result = $conn->query($sql);
+	while($result=$sql_slt_cidades_preparado->fetch()){
+		$codigo_cidade=$sql_slt_cidades_dados['id'];
+		$nome_cidade=$sql_slt_cidades_dados['nome'];
+		echo "<option value='$codigo_cidade'>$nome_cidade</option>";
+	}
 //senão, quer dizer que a pessoa escolheu um estado válido, então faz a pesquisa das cidades dele.
 }else{
 	$sql_slt_cidades="SELECT * FROM cidades WHERE estados_id='$cod'";
